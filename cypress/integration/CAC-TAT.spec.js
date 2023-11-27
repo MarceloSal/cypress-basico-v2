bf8b07d574b0b6a35e6a2abb129f662675c22a31
MarceloSal <salmeron.marcelo@gmail.com>
2023-11-27 08:12:08 -0300

    aula 3 -'have.value'

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -15,15 +15,90 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     })
 
-    it.only('preenche os campos obrigatórios e envia o formulário', function(){
+    it('preenche os campos obrigatórios e envia o formulário', function(){
         const longText = "teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste, teste"
         cy.get('#firstName').type('Marcelo')
         cy.get('#lastName').type('Salmeron')
+        cy.get('#email').type('salmeron.marcelo@gmail,com')
+        cy.get('#open-text-area').type('Teste')
+        cy.contains('button', 'Enviar').click()
+
+        
+        cy.get('.error').should('be.visible')
+    })
+
+    it('campo telefone continua vazio, quando não preenchido com valor numérico', function(){
+        cy.get('#phone')
+            .type('abfdsajfbjdfbdj')
+            .should('have.value', '')
+    })
+
+    it('campo telefone continua vazio, quando não preenchido com valor numérico', function(){
+        cy.get('#firstName').type('Marcelo')
+        cy.get('#lastName').type('Salmeron')
         cy.get('#email').type('salmeron.marcelo@gmail.com')
-        cy.get('#open-text-area').type(longText)
-        cy.get('button[type="submit"]').click()
+        cy.get('#phone-checkbox').click()
+        cy.get('#open-text-area').type('Teste')
+        cy.contains('button', 'Enviar').click()
+
+        
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function(){
+        cy.get('#firstName')
+            .type('Marcelo')
+            .should('have.value', 'Marcelo')
+            .clear()
+            .should('have.value', '')
+        cy.get('#lastName')
+            .type('Salmeron')
+            .should('have.value', 'Salmeron')
+            .clear()
+            .should('have.value', '')   
+        cy.get('#email')
+            .type('salmeron.marcelo@gmail.com')
+            .should('have.value', 'salmeron.marcelo@gmail.com')
+            .clear()
+            .should('have.value', '')  
+        cy.get('#phone')
+            .type('123456789')
+            .should('have.value', '123456789')
+            .clear()
+            .should('have.value', '')       
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function(){
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formuário com sucesso usando um comando customizado', function(){
+        cy.fillMandatoryFieldsAndSubmit()
 
         cy.get('.success').should('be.visible')
     })
+
+    it('seleciona um produto (YouTube) por seu texto', function(){
+        cy.get('#product')
+        .select('YouTube')
+        .should('have.value', 'youtube')
+        // cy.get('.success').should('be.visible')
+    })
+
+    it('seleciona um produto (Mentoria) por seu Valor', function(){
+        cy.get('#product')
+        .select('mentoria')
+        .should('have.value', 'mentoria')
+        // cy.get('.success').should('be.visible')
+    })
+
+    it.only('seleciona um produto (blog), por seu índice', function(){
+        cy.get('#product')
+        .select(1)
+        .should('have.value', 'blog')
+    })
+
 })
   
